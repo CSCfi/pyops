c2dc063fef6e1b9f26cab44455b103838329a629
Kalle Happonen <kalle.happonen@csc.fi>
2018-07-17 15:18:04 +0300

    Downtime by hostgroup
    Made it possible to set downtime by hostgroup. This sets downtime for
    the servers individually, not the group.

--- set_downtime.py
+++ set_downtime.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 #
 # Set downtime for hosts
 #
@@ -23,14 +23,16 @@ def main(argv=None):
                                      epilog="""Examples:
 ./set_downtime.py -e +1d testserver1 testserver2
 ./set_downtime.py -s '2018-08-10 08:00' -c 'Server upgrade and maintenance' -e +8h prodserver1
-./set_downtime.py -d testserver1 testserver2 prodserver1""")
+./set_downtime.py -d testserver1 testserver2 prodserver1
+./set_downtime.py -g -e +1d 'compute node group with spaces' 'compute node group 2'""")
 
-    parser.add_argument('hosts', nargs='*', help="A space separated list of hosts to apply the downtime to.")
+    parser.add_argument('hosts', nargs='*', help="A space separated list of hosts (or hostgroups, see -g) to apply the downtime to.")
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-e', '--endtime', dest="endtime", help="Downtime end time in an opsview format (e.g. +10m, +2d or '2018-08-10 15:00').")
     group.add_argument('-d', '--delete', dest="delete", action="store_true", help="Delete downtimes instead of adding them.")
     parser.add_argument('-c', '--comment', dest="comment", default="Dowtime set by pyops", help="Comment for the downtime")
     parser.add_argument('-s', '--starttime', dest="starttime", default="now", help="Start time in an opsview format ('2018-08-10 13:00'). Defaults to 'now'")
+    parser.add_argument('-g', '--group', dest="group", action="store_true", help="Interpret the host list as a list of hostgroup names, and apply the downtime to all the hosts in the groups.")
 
     prod_base = "https://mon-esp.csc.fi/opsview/rest/"
     try:
@@ -46,19 +48,29 @@ def main(argv=None):
     args = parser.parse_args(argv[1:])
     if len(args.hosts) < 1:
         parser.print_help()
-        print 'Error: Specify one or more hosts'
+        print 'Error: Specify one or more hosts/hostgroups'
         return 1
 
     opsprod = pyops.opsview(prod_user, prod_pass, prod_base)
-    if args.endtime:
-        opsprod.set_host_downtime(args.hosts, args.starttime, args.endtime, args.comment)
-    if args.delete:
-        opsprod.remove_host_downtime(args.hosts)
+ 
+    if args.group:
+        hostlist = []
+        for host in args.hosts:
+            res = opsprod.get_hostgroup_by_name(host)
+            if not res:
+                print "Warning: could not find hostgroup '%s'" % host
+            else:
+                hltemp = [item["name"] for item in res["object"]["hosts"]]
+                hostlist.extend(hltemp)
 
-#for host in hosts:
-#    print "Setting downtime for host " + host
-    #opsprod.set_host_downtime([host], "now", "+10m", "Test by kalle")
-#    opsprod.remove_host_downtimes(hosts)
+    else:
+        hostlist = args.hosts
+
+    if args.endtime:
+        opsprod.set_host_downtime(hostlist, args.starttime, args.endtime, args.comment)
+    if args.delete:
+        opsprod.remove_host_downtime(hostlist)
+
 
 if __name__ == "__main__":
     main()
